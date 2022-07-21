597fe6c774e68abc11cf8160e79595868971997b
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-21 13:53:00 +0300

    Beginning of work - 2

--- src/index.js
+++ src/index.js
@@ -12,19 +12,34 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
+//! old
+// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3'; //! old
+// const BASE_URL = 'https://newsapi.org/v2'; //! old
 
-const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
-const BASE_URL = 'https://newsapi.org/v2';
-const options = {
-    headers: {
-        Authorization: API_KEY,
-    },
-};
+
+const API_KEY = '28759369-3882e1068ac26fe18d14affeb';
+const BASE_URL = 'https://pixabay.com/api/';
+
+// const options = {
+//     headers: {
+//         Authorization: API_KEY,
+//     },
+// };
+
 const q = "cat";
-const page = 3;
+const page = 1;
+const per_page = 5;
 
-const url = `${BASE_URL}/everything?q=${q}&language=en&pageSize=5&page=${page}`;
+
+// const url = `${BASE_URL}everything?q=${q}&language=en&pageSize=5&page=${page}`; //! old
+
+
+// https://pixabay.com/api/?key=28759369-3882e1068ac26fe18d14affeb&q=yellow+flowers&image_type=photo //! Example
+// fetch('https://pixabay.com/api/?key=28759369-3882e1068ac26fe18d14affeb&q=yellow+flowers&image_type=photo'); //! Example
+
+const url = `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&page=${page}&per_page=${per_page}`;
+
 console.log(url);
 
 
-fetch(url, options)
+fetch(url);
